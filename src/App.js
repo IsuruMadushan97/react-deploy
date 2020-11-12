a8acbbd59000d934e8b6dace4e4fdd00340f4db5
IsuruMadushan97 <54880109+IsuruMadushan97@users.noreply.github.com>
2020-11-12 12:35:33 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import profileImage from './img/profile-pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Blood donation website testing</h1>
+      <h2>Author : Isuru madushan</h2>
+      <img src={profileImage} alt="profile-image"/>
     </div>
   );
 }
